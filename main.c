a9883dc6d0717f0789e69dd99c63b719a458d732
Emilio J. Padrón González <emilioj@udc.gal>
2024-10-07 15:58:30 +0200

    Enunciado e esqueleto de código para Práctica 3

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include "MKL46Z4.h"
+#include "lcd.h"
 
 // LED (RG)
-// LED_GREEN = PTD5
-// LED_RED = PTE29
+// LED_GREEN = PTD5 (pin 98)
+// LED_RED = PTE29 (pin 26)
+
+// SWICH
+// RIGHT (SW1) = PTC3 (pin 73)
+// LEFT (SW2) = PTC12 (pin 88)
+
+// Enable IRCLK (Internal Reference Clock)
+// see Chapter 24 in MCU doc
+void irclk_ini()
+{
+  MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
+  MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
+}
 
 void delay(void)
 {
@@ -11,41 +24,37 @@ void delay(void)
   for (i = 0; i < 1000000; i++);
 }
 
-// LED_GREEN = PTD5
-void led_green_init()
-{
-  // SIM->COPC
-  // SIM->SCGC5
-  // PORTD->PCR[5]
-  // GPIOD->PDDR
-  // GPIOD->PSOR
-}
-
-void led_green_toggle()
-{
-  //
-}
-
-// LED_RED = PTE29
-void led_red_init()
-{
-  //
-}
-
-void led_red_toggle(void)
-{
-  //
-}
-
 int main(void)
 {
-  led_green_init();
-  led_red_init();
+  irclk_ini(); // Enable internal ref clk to use by LCD
+
+  lcd_ini();
+  lcd_display_dec(666);
+
+  // 'Random' sequence :-)
+  volatile unsigned int sequence = 0x32B14D98,
+    index = 0;
+
+  while (index < 32) {
+    if (sequence & (1 << index)) { //odd
+      //
+      // Switch on green led
+      // [...]
+      //
+    } else { //even
+      //
+      // Switch on red led
+      // [...]
+      //
+    }
+    // [...]
+  }
+
+  // Stop game and show blinking final result in LCD: hits:misses
+  // [...]
+  //
 
   while (1) {
-    led_green_toggle();
-    delay();
-    led_red_toggle();
   }
 
   return 0;
